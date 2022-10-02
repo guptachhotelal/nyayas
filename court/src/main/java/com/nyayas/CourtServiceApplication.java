7fb73f34fca1b49f9c21e13d0cf87795114ec2b3
chhotelal gupta <5537238+guptachhotelal@users.noreply.github.com>
2022-10-02 21:05:07 +0530

    Updated Java version from 1.8 to 17

--- court/src/main/java/com/nyayas/CourtServiceApplication.java
+++ court/src/main/java/com/nyayas/CourtServiceApplication.java
@@ -2,11 +2,11 @@ package com.nyayas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableDiscoveryClient
 @SpringBootApplication
